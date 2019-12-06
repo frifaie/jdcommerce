8e51a331d2741433c8616d29b06bd10aa11e7f51
Farid Rifaie <frifaie15@gmail.com>
2019-12-06 15:46:07 +0700

    add cart items

--- core/models.py
+++ core/models.py
@@ -23,18 +23,26 @@ class Item (models.Model):
     slug = models.SlugField()
     description = models.TextField()
 
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("core:product", kwargs={'slug': self.slug })
 
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={'slug': self.slug})
+
 
 class OrderItem(models.Model):
-    items = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{ self.quantity } of { self.item.title }"
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
